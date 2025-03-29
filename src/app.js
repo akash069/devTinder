52064b4bbcba3bd02fa6e81d230ba0b8df291434
akashkc069 <akashkc069@gmail.com>
2025-03-29 20:51:25 +0530

    Multiplse res using next

--- src/app.js
+++ src/app.js
@@ -3,9 +3,20 @@ const express = require('express');
 const app = express();
 
 //request handler
-app.use("/test",(req,res) => {
-   res.send("Hello from kc");  
-});
+app.use("/user",(req,res,next) => {
+   //Rout Handler
+   //res.send("Rout Handler 2");
+   next();
+   //next is express fun that call the res fun  
+},
+(req,res,next)=>{
+   //res.send("2nd Response!!");
+   next();
+},
+(req,res,next)=>{
+   res.send("3nd Response!!");
+}
+);
 
 app.listen(3000, () =>{
  console.log("server is succesfully listening on port 3000..");   
